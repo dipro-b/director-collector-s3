f381d955f166090dc45cd4b3663eb7d7124def42
Dipro Bhowmik <dipro.bhowmik@gmail.com>
2017-07-01 22:44:31 -0400

    Initial commit

---
+++ src/com/trustee/main/Trustee.java
@@ -0,0 +1,31 @@
+package com.trustee.main;
+
+public class Trustee {
+	private String myName;
+	private String myJob;
+	
+	public Trustee() {
+		
+	}
+	
+	public Trustee(String name, String job) {
+		this.myName = name;
+		this.myJob = job;
+	}
+	
+	public String getName() {
+		return this.myName;
+	}
+	
+	public String getJob() {
+		return this.myJob; 
+	}
+	
+	public void setName(String name) {
+		this.myName = name;
+	}
+	
+	public void setJob(String job) {
+		this.myJob = job;
+	}
+}
